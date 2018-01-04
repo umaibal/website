38cf3204a64bfc7871c57116807161faee4fd5e3
umaibal <umai.balendra@shopify.com>
2018-01-04 12:04:07 -0500

    using w3 buttons for sidebar menu

---
+++ jscript.js
@@ -0,0 +1,14 @@
+//use javascript for the sidebar to have
+//reactions based on user interaction
+
+
+//function to open sidebar once hamburger is clicked
+function _open() {
+document.getElementById('sidebar').style.display = "block";
+}
+
+
+//function to close the sidebar after opened by user
+function _close() {
+document.getElementById('sidebar').style.display = "none";
+}
